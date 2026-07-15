f0348dad689a40b146b6a17668be5fde159ac87f
Thibaud Colas <thibaudcolas@gmail.com>
2026-07-15 07:16:48 +0200

    Release v6.0.0

--- draftjs_exporter/__init__.py
+++ draftjs_exporter/__init__.py
@@ -6,7 +6,7 @@ type aliases, and helpers so callers can import everything from
 """
 
 __title__ = "draftjs_exporter"
-__version__ = "5.2.0"
+__version__ = "6.0.0"
 __uri__ = "https://wagtail.github.io/draftjs_exporter/"
 __author__ = "Springload and Contributors"
 __license__ = "MIT"
