97e888abc1e2309259364434e159be394a46c28c
Thibaud Colas <thibaudcolas@gmail.com>
2026-08-04 09:34:56 +0200

    Add link destination escaping and code delimiter sizing

--- tests/markdown/test_escape.py
+++ tests/markdown/test_escape.py
@@ -2,7 +2,12 @@
 
 import unittest
 
-from draftjs_exporter.markdown.escape import escape_text
+from draftjs_exporter.markdown.escape import (
+    code_block_fence,
+    code_span_delimiters,
+    escape_link_destination,
+    escape_text,
+)
 
 
 class TestEscapeTextAnywhere(unittest.TestCase):
@@ -66,6 +71,63 @@ class TestEscapeTextLineStart(unittest.TestCase):
 
     def test_empty_string(self):
         self.assertEqual(escape_text("", at_line_start=True), "")
+
+
+class TestEscapeLinkDestination(unittest.TestCase):
+    def test_plain_url_unchanged(self):
+        self.assertEqual(
+            escape_link_destination("https://example.com/a?b=1&c=2"),
+            "https://example.com/a?b=1&c=2",
+        )
+
+    def test_parentheses_escaped(self):
+        self.assertEqual(
+            escape_link_destination("https://example.com/a_(b)"),
+            "https://example.com/a_\\(b\\)",
+        )
+
+    def test_backslash_escaped(self):
+        self.assertEqual(escape_link_destination("a\\b"), "a\\\\b")
+
+    def test_space_percent_encoded(self):
+        self.assertEqual(escape_link_destination("a b"), "a%20b")
+
+    def test_control_char_percent_encoded(self):
+        self.assertEqual(escape_link_destination("a\nb"), "a%0Ab")
+
+    def test_non_ascii_unchanged(self):
+        self.assertEqual(escape_link_destination("/café"), "/café")
+
+
+class TestCodeSpanDelimiters(unittest.TestCase):
+    def test_plain_content(self):
+        self.assertEqual(code_span_delimiters("foo"), ("`", "`"))
+
+    def test_backtick_in_content(self):
+        self.assertEqual(code_span_delimiters("a`b"), ("``", "``"))
+
+    def test_two_backticks_in_content(self):
+        self.assertEqual(code_span_delimiters("a``b"), ("```", "```"))
+
+    def test_leading_backtick_padded(self):
+        self.assertEqual(code_span_delimiters("`x"), ("`` ", " ``"))
+
+    def test_trailing_backtick_padded(self):
+        self.assertEqual(code_span_delimiters("x`"), ("`` ", " ``"))
+
+
+class TestCodeBlockFence(unittest.TestCase):
+    def test_plain_content(self):
+        self.assertEqual(code_block_fence("foo\n", "`"), "```")
+
+    def test_fence_run_in_content(self):
+        self.assertEqual(code_block_fence("a```b\n", "`"), "````")
+
+    def test_minimum_length_three(self):
+        self.assertEqual(code_block_fence("a`b\n", "`"), "```")
+
+    def test_tilde_fence(self):
+        self.assertEqual(code_block_fence("a~~~b\n", "~"), "~~~~")
 
 
 if __name__ == "__main__":
